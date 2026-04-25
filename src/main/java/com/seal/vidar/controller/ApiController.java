94b77c82ea1497fd58dec4ec656733494f7e2fce
seal <3291351273@qq.com>
2026-04-25 11:07:54 +0800

    fix: restore question list in API response and clean up DI
    - Uncomment setQuestionList() call that caused API to always return empty data
    - Replace field injection with constructor injection in ApiController
    - Remove unused imports (Questions, MongoTemplate, Autowired)

--- src/main/java/com/seal/vidar/controller/ApiController.java
+++ src/main/java/com/seal/vidar/controller/ApiController.java
@@ -1,9 +1,7 @@
 package com.seal.vidar.controller;
 
-import com.seal.vidar.entity.bo.Questions;
 import com.seal.vidar.entity.dto.QuestionsDto;
 import com.seal.vidar.service.ApiService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,8 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class ApiController {
 
-    @Autowired
-    private ApiService apiService;
+    private final ApiService apiService;
+
+    public ApiController(ApiService apiService) {
+        this.apiService = apiService;
+    }
 
     @GetMapping("/questions")
     public ResponseEntity<QuestionsDto> getQuestions() {
